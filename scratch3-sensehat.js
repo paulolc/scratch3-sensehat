a7dc8871eed6b72f5e32dd9104c6134c70b8c6e9
Paulo Carvalho <plcarvalho@gmail.com>
2020-02-18 22:21:56 +0000

    Updated for scratch3-bridge v2

--- scratch3-sensehat.js
+++ scratch3-sensehat.js
@@ -2,21 +2,25 @@ const ledmatrix = require('node-sense-hat').Leds;
 const ScratchBridge = require('scratch3-bridge');
 
 let s3b = new ScratchBridge();
+let sender = 0;
 
 ledmatrix.clear();
 ledmatrix.setRotation(270);
 
 let starting = true;
-s3b.on('msg', msg =>{
-    let values = msg.msg;
-    let data = values.map( x => parseInt(x) );
-    console.log(`> ${values}`);
+s3b.on('data', (msg,asender) =>{
+    sender = asender;
+    let values = msg;
+    console.log(`values: (${sender})> ${values}`);
 
     if (!starting ){
-        if(values===""){
+        if(!values){
             ledmatrix.clear();
+            return;
         }
+        let data = values.map( x => parseInt(x) );
         let row = 8 - Math.ceil( data[0] / 8 ) ;
+
         let column = (data[0] - 1) % 8  ;
         let rgbcolor = [ data[1] ,data[2], data[3] ];
         ledmatrix.setPixel( row , column, rgbcolor );
@@ -30,3 +34,4 @@ s3b.on('connect', ()=>{
 })
 
 s3b.connect();
+
